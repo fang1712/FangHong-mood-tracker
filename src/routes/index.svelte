2ba73d9374cd1540076a0ef56e02eaa9d0c3e1e2
fang1712 <fanghong1712@gmail.com>
2022-01-09 04:10:48 +0000

    Moody Tracking

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import supabase from '$lib/db';
+import Login from '$lib/login.svelte';
+import Signup from '$lib/signup.svelte';
+    
+
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>	
+
+<Signup/>
+<Login/>
